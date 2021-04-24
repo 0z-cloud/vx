819d2f437a1da4a38f007c2a853ec873a7d9b2a8
root <root@user-PC.localdomain>
2021-04-24 22:02:49 +0300

    devops(clear): make right run

--- services/codecloudcoin/local_bootstrap_and_rebuld.sh
+++ services/codecloudcoin/local_bootstrap_and_rebuld.sh
@@ -27,11 +27,13 @@ tar -ch ../codecloudcoin | docker build -t woinc/api-labirint:latest -f ./codecl
 #################
 cd $woinc_api_path
 
-docker rm -f woinc-api-postgresql
-docker rm -f woinc-api-service
+docker ps -a | awk '{print $1}' | tail -n +2 | xargs -I ID docker rm -f ID
 
-docker run -d --name woinc-api-postgresql -p 5432:5432 -e POSTGRES_PASSWORD=7539148620qQ -e POSTGRES_DB=woincapi postgres
+# docker rm -f woinc-api-postgresql
+# docker rm -f woinc-api-service
+
+docker run -d --privileged --name woinc-api-postgresql -p 5432:5432 -v data:/var/lib/postgresql/data -e POSTGRES_PASSWORD=7539148620qQ -e POSTGRES_DB=woincapi postgres
 echo "Sleep wait when postgres UP"
 sleep 50
 # docker run -d --name woinc-api-service -p 4444:4444 -e POSTGRES_PASSWORD=7539148620qQ --link woinc-api-postgresql:woinc-api-postgresql -e POSTGRES_DB=woincapi -v $projects_path:/codecloudcoin/woinc woinc/api-labirint:latest
-docker run -d --name woinc-api-service -p 4444:4444 -e POSTGRES_PASSWORD=7539148620qQ --link woinc-api-postgresql:woinc-api-postgresql -e POSTGRES_DB=woincapi woinc/api-labirint:latest
+docker run -d --privileged --name woinc-api-service -p 4444:4444 -e POSTGRES_PASSWORD=7539148620qQ --link woinc-api-postgresql woinc-api-postgresql -e POSTGRES_DB=woincapi woinc/api-labirint:latest
