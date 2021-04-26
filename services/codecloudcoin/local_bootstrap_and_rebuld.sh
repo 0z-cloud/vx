8c007667a2e7fe6702888575834e62159c2f5343
root <root@user-PC.localdomain>
2021-04-26 03:08:55 +0300

    up(fix): run run run

--- services/codecloudcoin/local_bootstrap_and_rebuld.sh
+++ services/codecloudcoin/local_bootstrap_and_rebuld.sh
@@ -109,6 +109,7 @@ sleep 25
 
 echo -e "a/Wait when database are done Database is complete bootstrap"
 sleep 50
+echo "docker exec woinc-db /bin/su postgres -c '/db_create.sh ${DATABASE_USER} ${DATABASE_PASSWORD}'"
 docker exec woinc-db /bin/su postgres -c '/db_create.sh ${DATABASE_USER} ${DATABASE_PASSWORD}'
 
 echo -e "Create the Application Container Instance ..."
